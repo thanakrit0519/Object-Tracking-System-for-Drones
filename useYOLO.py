66ff9f362e8615c12fbb893a67a9e708f25ea8df
thanakrit0519 <52774191+thanakrit0519@users.noreply.github.com>
2024-08-08 10:41:14 +0700

    -

--- useYOLO.py
+++ useYOLO.py
@@ -13,28 +13,28 @@ object_class = [
   'awning-tricycle',
   'bus',
   'motor']
-GREEN = (0, 255, 0) 
-model = YOLO("/home/song/CoopProject/trained_model_visdrone2019_1_7_2024/yolov8x-p6.pt")
-def predict(chosen_model, img, classes=[], conf=0.5):
-    if classes:
-        results = chosen_model.predict(img, classes=classes, conf=conf)
-    else:
-        results = chosen_model.predict(img, conf=conf)
-
+BLUE = (255, 0, 0) 
+model = YOLO("trained_model_visdrone2019_1_7_2024\yolov8l-p2.pt")
+cap = cv2.VideoCapture(0)
+w=cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+h=cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+print(w,h)
+def predict(chosen_model, img, conf=0.1):
+    results = chosen_model.predict(img, conf=conf,imgsz=(h,w))
     return results
 
-def predict_and_detect(chosen_model, img, classes=[], conf=0.5):
-    results = predict(chosen_model, img, classes, conf=conf)
+def predict_and_detect(chosen_model, img, conf=0.5):
+    results = predict(chosen_model, img, conf=conf)
 
     for result in results:
         for box in result.boxes:
             cv2.rectangle(img, (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
-                          (int(box.xyxy[0][2]), int(box.xyxy[0][3])), (255, 0, 0), 2)
+                          (int(box.xyxy[0][2]), int(box.xyxy[0][3])), BLUE, 2)
             cv2.putText(img, f"{result.names[int(box.cls[0])]}",
                         (int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),
-                        cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+                        cv2.FONT_HERSHEY_PLAIN, 2, BLUE, 2)
     return img, results
-cap = cv2.VideoCapture(0)
+
 while True:
     ret, image = cap.read()
 
